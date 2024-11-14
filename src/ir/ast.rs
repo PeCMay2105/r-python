66a97cf404805fa4c419a99d2291c9fb0f664fe4
EMERSON LUIZ CRUZ JUNIOR <emersonjr02@gmail.com>
2024-11-14 08:58:01 -0300

    testing the commit in my fork

--- src/ir/ast.rs
+++ src/ir/ast.rs
@@ -52,6 +52,7 @@ pub enum Statement {
     While(Box<Expression>, Box<Statement>),
     Block(Vec<Statement>), // For indented blocks
     Sequence(Box<Statement>, Box<Statement>),
+    Casting(Box<Name>, Box<Expression>),
 }
 
 #[derive(Debug)]
